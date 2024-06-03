2e81abea4b51e62a2f6c44739544b21c558fe47c
kseniakopteva <ksenija.kopteva@gmail.com>
2024-06-03 13:51:46 +0300

    added age, school and interests to profile view

--- app/src/main/java/com/example/studybuddy/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/studybuddy/ui/profile/ProfileFragment.kt
@@ -1,10 +1,12 @@
 package com.example.studybuddy.ui.profile
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.iterator
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -14,6 +16,7 @@ import androidx.navigation.fragment.findNavController
 import com.example.studybuddy.R
 import com.example.studybuddy.databinding.FragmentProfile1Binding
 import com.example.studybuddy.util.Resource
+import com.google.android.material.chip.Chip
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -78,6 +81,28 @@ class ProfileFragment : Fragment() {
                             val user = response.data
                             binding.userName.text = user?.firstName
                             binding.description.text = user?.description
+                            binding.userSurname.text = user?.lastName
+                            binding.userYear.text = buildString {
+                                append("Vecums:")
+                                append(user?.tags?.findLast { it.type == "age" }?.name)
+                            }
+                            binding.userSchool.text = user?.tags?.findLast { it.type == "establishment" }?.name
+                            Log.i("All tags", user?.tags.toString())
+                            val tagsInt = user?.tags?.filter { it.type == "interests" }
+                            if (tagsInt != null) {
+                                for (interest in tagsInt.iterator()) {
+                                    val chip = Chip(requireContext()).apply {
+                                        text = interest.name
+                                        setChipStrokeWidthResource(R.dimen.border)
+                                        setChipStrokeColorResource(R.color.orange_100)
+                                        chipStrokeColor = resources.getColorStateList(R.color.orange_100, null)
+                                        chipCornerRadius = 16f
+                                        chipBackgroundColor = resources.getColorStateList(R.color.orange_50, null)
+                                        background = resources.getDrawable(R.drawable.bg_et, null)
+                                    }
+                                    binding.chipGroupInterestsProfile.addView(chip)
+                                }
+                            }
                         }
                         is Resource.Error -> {
                             Toast.makeText(requireContext(), "Failed to fetch user", Toast.LENGTH_SHORT).show()
